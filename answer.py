91530d53b8b764a15431b1d2518c6aab20181f96
Idris isah <81864601+maheer101@users.noreply.github.com>
2021-04-05 02:08:03 +0100

    Add files via upload

---
+++ answer.py
@@ -0,0 +1,35 @@
+def personal_details():
+    name, = "idris isah",
+    department= "computer science"
+    print("Name: {}\ndepartment: {}".format(name, department))
+personal_details()
+
+scare = 'Boo!'
+print ('scare:{}' .format(scare))
+
+b = 7
+c= 7
+d = b+c
+print('d:{}'.format(d))
+
+x=12
+y=.5
+outcome=x/y
+print('outcome:{}'.format(outcome))
+
+a=5
+b=5
+sum=a+b
+print('sum:{}'.format(sum))
+
+x = 8
+y = 3
+remainder= x % y
+print ('remainder:{}'.format(remainder))
+
+a=4
+b=2
+c=a*b
+d=a+b
+e=c*d
+print('e:{}'.format(e))
